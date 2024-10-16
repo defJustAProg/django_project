44080c8b12ae37589a929cb92958de415d416ef5
Roman <roman.worker.email@mail.ru>
2024-10-16 16:36:02 +0500

    add class-based views

--- venv/app1/urls.py
+++ venv/app1/urls.py
@@ -1,8 +1,60 @@
 from django.urls import path
-from app1.views import index, url2, url3
+from .views import (
+    index,
+    url2,
+    url3,
+    CustomerListView,
+    CustomerDetailView,
+    CustomerCreateView,
+    CustomerUpdateView,
+    CustomerDeleteView,
+    ProductListView,
+    ProductDetailView,
+    ProductCreateView,
+    ProductUpdateView,
+    ProductDeleteView,
+    OrderListView,
+    OrderDetailView,
+    OrderCreateView,
+    OrderUpdateView,
+    OrderDeleteView,
+    StorageListView,
+    StorageDetailView,
+    StorageCreateView,
+    StorageUpdateView,
+    StorageDeleteView,
+)
 
 urlpatterns = [
     path('', index),
     path('url2/', url2),
-    path('url3/', url3)
+    path('url3/', url3),
+
+    # Customer URLs
+    path('customers/', CustomerListView.as_view(), name='customer_list'),
+    path('customers/<str:pk>/', CustomerDetailView.as_view(), name='customer_detail'),
+    path('customers/create/', CustomerCreateView.as_view(), name='customer_create'),
+    path('customers/update/<str:pk>/', CustomerUpdateView.as_view(), name='customer_update'),
+    path('customers/delete/<str:pk>/', CustomerDeleteView.as_view(), name='customer_delete'),
+
+    # Product URLs
+    path('products/', ProductListView.as_view(), name='product_list'),
+    path('products/<uuid:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('products/create/', ProductCreateView.as_view(), name='product_create'),
+    path('products/update/<uuid:pk>/', ProductUpdateView.as_view(), name='product_update'),
+    path('products/delete/<uuid:pk>/', ProductDeleteView.as_view(), name='product_delete'),
+
+    # Order URLs
+    path('orders/', OrderListView.as_view(), name='order_list'),
+    path('orders/<int:pk>/', OrderDetailView.as_view(), name='order_detail'),
+    path('orders/create/', OrderCreateView.as_view(), name='order_create'),
+    path('orders/update/<int:pk>/', OrderUpdateView.as_view(), name='order_update'),
+    path('orders/delete/<int:pk>/', OrderDeleteView.as_view(), name='order_delete'),
+
+    # Storage URLs
+    path('storages/', StorageListView.as_view(), name='storage_list'),
+    path('storages/<str:pk>/', StorageDetailView.as_view(), name='storage_detail'),
+    path('storages/create/', StorageCreateView.as_view(), name='storage_create'),
+    path('storages/update/<str:pk>/', StorageUpdateView.as_view(), name='storage_update'),
+    path('storages/delete/<str:pk>/', StorageDeleteView.as_view(), name='storage_delete'),
 ]
--- venv/app1/views.py
+++ venv/app1/views.py
@@ -9,3 +9,104 @@ def url2(request):
 
 def url3(request):
     return HttpResponse("url3")
+
+
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Customer, Product, Order, Storage
+
+# Customer Views
+class CustomerListView(ListView):
+    model = Customer
+    template_name = 'customer_list.html'
+
+class CustomerDetailView(DetailView):
+    model = Customer
+    template_name = 'customer_detail.html'
+
+class CustomerCreateView(CreateView):
+    model = Customer
+    template_name = 'customer_form.html'
+    fields = ['login', 'email']
+
+class CustomerUpdateView(UpdateView):
+    model = Customer
+    template_name = 'customer_form.html'
+    fields = ['login', 'email']
+
+class CustomerDeleteView(DeleteView):
+    model = Customer
+    template_name = 'customer_confirm_delete.html'
+    success_url = reverse_lazy('customer_list')
+
+# Product Views
+class ProductListView(ListView):
+    model = Product
+    template_name = 'product_list.html'
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'product_detail.html'
+
+class ProductCreateView(CreateView):
+    model = Product
+    template_name = 'product_form.html'
+    fields = ['name', 'description', 'count_of_products_on_warehouse', 'price', 'image', 'storage']
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    template_name = 'product_form.html'
+    fields = ['name', 'description', 'count_of_products_on_warehouse', 'price', 'image', 'storage']
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'product_confirm_delete.html'
+    success_url = reverse_lazy('product_list')
+
+# Order Views
+class OrderListView(ListView):
+    model = Order
+    template_name = 'order_list.html'
+
+class OrderDetailView(DetailView):
+    model = Order
+    template_name = 'order_detail.html'
+
+class OrderCreateView(CreateView):
+    model = Order
+    template_name = 'order_form.html'
+    fields = ['product', 'customer', 'status_of_delivery', 'feedback', 'count_of_positions']
+
+class OrderUpdateView(UpdateView):
+    model = Order
+    template_name = 'order_form.html'
+    fields = ['product', 'customer', 'status_of_delivery', 'feedback', 'count_of_positions']
+
+class OrderDeleteView(DeleteView):
+    model = Order
+    template_name = 'order_confirm_delete.html'
+    success_url = reverse_lazy('order_list')
+
+# Storage Views
+class StorageListView(ListView):
+    model = Storage
+    template_name = 'storage_list.html'
+
+class StorageDetailView(DetailView):
+    model = Storage
+    template_name = 'storage_detail.html'
+
+class StorageCreateView(CreateView):
+    model = Storage
+    template_name = 'storage_form.html'
+    fields = ['name']
+
+class StorageUpdateView(UpdateView):
+    model = Storage
+    template_name = 'storage_form.html'
+    fields = ['name']
+
+class StorageDeleteView(DeleteView):
+    model = Storage
+    template_name = 'storage_confirm_delete.html'
+    success_url = reverse_lazy('storage_list')
